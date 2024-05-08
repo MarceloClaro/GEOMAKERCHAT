dee4614920464007616b35b44e391b7061493fb4
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-05-08 08:00:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_groq import ChatGroq
 
 def upload_data():
     """
-    Permite aos usuários fazer upload de arquivos JSON e CSV, armazenando-os no estado da sessão.
+    Permite aos usuários fazer upload de arquivos JSON e CSV.
     """
     uploaded_files = st.file_uploader("Faça upload dos seus arquivos (JSON ou CSV, até 300MB cada)", type=['json', 'csv'], accept_multiple_files=True)
     data_frames = []
@@ -44,17 +44,18 @@ def main():
 
     groq_api_key = os.getenv('GROQ_API_KEY', 'Chave_API_Padrão')
     model_choice = st.sidebar.selectbox("Escolha um modelo", ["llama3-70b-8192", "llama3-8b-8192", "mixtral-8x7b-32768", "gemma-7b-it"])
-    memory = ConversationBufferWindowMemory(k=5, memory_key="chat_history")
-    data_frames = upload_data()
-
     max_tokens = st.sidebar.slider("Máximo de Tokens:", 100, 1000, 500, 50)
-    primary_prompt = st.sidebar.text_input("Prompt do sistema principal", "Como posso ajudar você hoje?")
-    secondary_prompt = st.sidebar.text_input("Prompt do sistema secundário", "Há algo mais em que posso ajudar?")
+    primary_prompt = "Como posso ajudar você hoje?"
+    secondary_prompt = "Há algo mais em que posso ajudar?"
+
+    # Check if there is already a prompt in session state, otherwise set the primary prompt
+    if 'last_prompt' not in st.session_state:
+        st.session_state.last_prompt = primary_prompt
 
     user_question = st.text_input("Faça uma pergunta:")
     if user_question:
-        current_prompt = secondary_prompt if memory.get_last_prompt() == primary_prompt else primary_prompt
-        memory.store_prompt(current_prompt)
+        current_prompt = secondary_prompt if st.session_state.last_prompt == primary_prompt else primary_prompt
+        st.session_state.last_prompt = current_prompt  # Update the last prompt in session state
 
         prompt = ChatPromptTemplate.from_messages([
             SystemMessage(content=current_prompt),
@@ -62,13 +63,13 @@ def main():
             HumanMessagePromptTemplate.from_template("{human_input}")
         ])
 
+        memory = ConversationBufferWindowMemory(k=5, memory_key="chat_history")
         conversation = LLMChain(llm=ChatGroq(api_key=groq_api_key, model_name=model_choice), prompt=prompt, memory=memory, max_tokens=max_tokens)
         response = conversation.predict(human_input=user_question)
         message = {'human': user_question, 'AI': response}
         st.session_state.chat_history.append(message)
         st.write("Chatbot:", response)
-
-        show_data_visualizations(data_frames)  # Display visualizations automatically after data is processed
+        show_data_visualizations(upload_data())  # Display visualizations automatically after data is processed
 
 if __name__ == "__main__":
     main()
