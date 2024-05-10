0e5be463ff87ca8fd1e543871d8ad745b3ff7fcc
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-05-10 11:34:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 from crewai import Agent, Task, Crew, Process
 from langchain_groq import ChatGroq
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from crewai_tools import DuckDuckGoSearchRun
 
 import toml
-import time  # Para adicionar um pequeno atraso entre as solicitações
+import time
 
 # Carregar a chave de API do Groq do arquivo secrets.toml
 secrets = toml.load("secrets.toml")
@@ -27,13 +28,48 @@ def main():
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
 
-    model_kwargs = {}
-    groq_chat = ChatGroq(api_key=groq_api_key, model_name=model_choice, **model_kwargs)
+    search_tool = DuckDuckGoSearchRun()
+    groq_chat = ChatGroq(api_key=groq_api_key, model_name=model_choice)
+
+    researcher = Agent(
+        role="Pesquisador Sênior",
+        goal="Descubra as três principais notícias sobre {topic}",
+        verbose=True,
+        memory=True,
+        backstory=(
+            "Como assistente de pesquisa dedicado a descobrir as tendências mais impactantes, você é movido por uma curiosidade implacável e um compromisso com a inovação. Sua função envolve aprofundar-se nos desenvolvimentos mais recentes em vários setores para identificar e analisar as principais notícias de tendência em qualquer campo. Essa busca não apenas satisfaz sua sede de conhecimento, mas também permite que você contribua com insights valiosos que podem potencialmente remodelar entendimentos e expectativas em escala global."
+        ),
+        tools=[search_tool],
+        allow_delegation=True,
+        llm=groq_chat
+    )
+
+    write_task = Task(
+        description=(
+            "Componha um artigo informativo sobre {topic}."
+            "Concentre-se nas últimas tendências e em como elas estão impactando a indústria."
+            "Este artigo deve ser fácil de entender, envolvente e positivo."
+        ),
+        expected_output="Um artigo de 4 parágrafos sobre os avanços de {topic} formatado como markdown traduzido para o português.",
+        tools=[search_tool],
+        agent=researcher,
+        async_execution=False,
+        output_file="blog-post.md"
+    )
+
+    crew = Crew(
+        agents=[researcher],
+        tasks=[write_task],
+        process=Process.sequential
+    )
 
     user_question = st.text_input("Faça uma pergunta:")
     if user_question:
-        prompt = f"{primary_prompt} {user_question} {secondary_prompt}"
-        conversation = ChatGroq(api_key=groq_api_key, model_name=model_choice, **model_kwargs)
+        current_prompt = secondary_prompt if 'last_prompt' in st.session_state and st.session_state.last_prompt == primary_prompt else primary_prompt
+        st.session_state.last_prompt = current_prompt
+
+        prompt = f"{current_prompt} {user_question}"
+        conversation = ChatGroq(api_key=groq_api_key, model_name=model_choice)
         response = conversation.predict(prompt)
         st.write("Chatbot:", response)
 
