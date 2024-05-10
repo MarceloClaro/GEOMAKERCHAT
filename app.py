9e8a72cadd80a95acf97f3324854fd33bd3f6ad5
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-05-10 17:19:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def main():
         verbose=True,
         memory=True,
         backstory=(
-            "作为文章评估者，您具有敏锐的分析能力和对学术研究过程的深刻理解。您的批判性分析突出了文章的优点，也指出了文章可能存在的缺陷，为持续改进学术研究质量做出了贡献。"
+            "作为文章评估者，您具有敏锐的分析能力和对学术研究过程的深刻理解。您的批判性分析不仅突出了文章的优点，还指出了文章可能存在的缺陷，为持续改进学术研究质量做出了贡献。"
         ),
         tools=[search_tool],
         allow_delegation=False,
@@ -162,8 +162,8 @@ def main():
                     st.warning(f"Limite de taxa excedido. Aguardando {retry_time_seconds} segundos antes de tentar novamente...")
                     time.sleep(retry_time_seconds)
 
-    # Exibir a contagem de tokens na barra lateral
-    st.sidebar.write(f"Tokens usados ({model}): {tokens_used[model]} de {rate_limits[model]}")
+        # Exibir a contagem de tokens na barra lateral
+        st.sidebar.write(f"Tokens usados ({model}): {tokens_used[model]} de {rate_limits[model]}")
 
 if __name__ == "__main__":
     main()
